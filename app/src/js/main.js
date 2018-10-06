40ffc30612519c017eab335094fd3c0766ced8bf
Stiven B <stivenb1994@gmail.com>
2018-10-06 17:55:58 -0500

    add index of colegio

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -1,24 +1,14 @@
 import styles from '../styles/main.scss';
 import '!file-loader?name=[name].[ext]!../../public/favicon.ico';
+import '../../public/media/img/black_school.jpg'
 import '../../public/views/basic.html';
+import './jquery.slides';
+import './script';
 var campos;
 
 window.onload = function() {
   campos = document.querySelector('.wrapper');
-  start(campos);
-  load();
   myPromises();
-}
-
-function load() {
-  const myImg = document.querySelector('.js-button');
-  fetch('https://upload.wikimedia.org/wikipedia/commons/7/77/Delete_key1.jpg')
-  .then((response) => response.blob())
-  .then((response) => {
-    const objectURL = URL.createObjectURL(response);
-    myImg.src = objectURL;
-  })
-  .catch((err) => console.log('no pudo: ' + err));
 }
 
 function myPromises() {
@@ -35,8 +25,3 @@ function myPromises() {
   });
 }
 
-function start(campo) {
-  var span = document.createElement('span');
-  span.textContent = "Super Genial"
-  campo.appendChild(span);
-}
--- theme/README.md
+++
@@ -1,2 +0,0 @@
-# pagina_colegio
-## Version: BETA (0.1)
